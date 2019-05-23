4b9c3144499d4876c1edc12426e8e396d787e471
7Aditya7 <41566597+7Aditya7@users.noreply.github.com>
2019-05-22 23:16:08 -0700

    Add files via upload

---
+++ lab 3(a).cpp
@@ -0,0 +1,20 @@
+//lab 3:passint arguments to function
+//(a)Program to demonstrate pass by value.
+#include<iostream>
+using namespace std;
+void decrease(int b)
+{
+    b=b-2;
+
+}
+int main()
+{
+    int x=20;
+    cout<<"Value of x before calling decrease()"<<endl;
+    cout<<x<<endl;
+    decrease(x);
+    cout<<"Value of x after calling decrease() is:"<<endl;
+    cout<<x;
+    return 0;
+
+}
